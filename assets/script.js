72764116615a16ca49be2c8eb47d5cd9f09d5340
nonebula <147166151+nonebula@users.noreply.github.com>
2023-11-16 20:50:32 +0000

    Added comments as 'to do' checklists for task

--- assets/script.js
+++ assets/script.js
@@ -91,16 +91,31 @@ var upperCasedCharacters = [
 // Function to prompt user for password options
 function getPasswordOptions() {
 
+
+//steps:
+//1. prompt to get user input for password
+//2. use confirm to ask whether to include others
+//3. validate user input (at least one character type selected)
+//4. return object containing user's chosen option/s
 }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
 
+//steps:
+//1. take an array as an argument and return random element from that array
+//2. use Math.random() to generate random index within array length
+//3. return element at randomly generated index
 }
 
 // Function to generate password with user input
 function generatePassword() {
 
+//steps:
+//1. call getPasswordOptions to get the user's chosen options
+//2. based on choices, concatenate the selected character arrays into a single array
+//3. use a loop to generate a password of the specified length, randomly selecting characters from the array
+//4. return the generated password.
 }
 
 // Get references to the #generate element
@@ -114,5 +129,19 @@ function writePassword() {
   passwordText.value = password;
 }
 
+
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
+
+//steps:
+//1. add event listener to genpwbutton so that it can call the writePassword function
+//2. inside function call generatePassword to get generated password
+//3. use document.querySelector to get reference to the password text area
+//4. set the value of the text area to the generated content
+
+
+
+//When done: 
+//test program
+//debug
+//launch and submit
