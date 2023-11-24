81a5ee8efa6561091505eadfa9983ac56a551b10
nonebula <147166151+nonebula@users.noreply.github.com>
2023-11-24 13:30:44 +0000

    generation of password and loop

--- assets/script.js
+++ assets/script.js
@@ -127,21 +127,38 @@ function getRandom(arr) {
 allCharacters
 //2. use Math.random() to generate random index within array length
 //3. return element at randomly generated index
-}
+
 
 // Function to generate password with user input
 function generatePassword() {
 //steps:
 //1. call getPasswordOptions to get the user's chosen options
-getPasswordOptions();
+var options = getPasswordOptions();
+if (!options) {
+// If options are not valid, return an empty string
+      return '';
+    }
 //2. based on choices, concatenate the selected character arrays into a single array
-var password = "";
-for (var i = 0; i < length; i++) {
-  var randomIndex = Math.floor(Math.random() * allCharacters.length);
-  password+= allCharacters.getRandom(arr);
-}
+var allCharacters = '';
+  if (options.includeLowercase) {
+    allCharacters += lowerCasedCharacters.join('');
+  }
+  if (options.includeUppercase) {
+    allCharacters += upperCasedCharacters.join('');
+  }
+  if (options.includeNumbers) {
+    allCharacters += numericCharacters.join('');
+  }
+  if (options.includeSpecials) {
+    allCharacters += specialCharacters.join('');
+  }
 //3. use a loop to generate a password of the specified length, randomly selecting characters from the array
+var password = '';
+for (var i = 0; i < options.length; i++) {
+  password += getRandom(allCharacters);
+}
 //4. return the generated password.
+return password;
 }
 
 // Get references to the #generate element all this below = done
@@ -155,19 +172,5 @@ function writePassword() {
   passwordText.value = password;
 }
 
-
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
-
-//steps:
-//1. add event listener to genpwbutton so that it can call the writePassword function
-//2. inside function call generatePassword to get generated password
-//3. use document.querySelector to get reference to the password text area
-//4. set the value of the text area to the generated content
-
-
-
-//When done: 
-//test program
-//debug
-//launch and submit
